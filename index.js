565ba032e07f8c5df9afc05933df63b88fad8b1c
SteRobWms <stephen.r.williams.87@gmail.com>
2020-07-30 19:21:07 -0500

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-function findMinAndRemove(array){
-
+function findMinAndRemove(array) {
+    let min = array[0]
+    let minIdx = 0
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] < min) {
+            min = array[i]
+            minIdx = i
+        }
+    }
+    array.splice(minIdx, 1)
+    return min
 }
 
-function selectionSort(array){
-
+function selectionSort(array) {
+    let sortedArr = []
+    // Use a "while" loop here because a "for" loop functionally increments count twice each cycle, instead of once. Increments first by reducing array length in findMinAndRemove. Increments second from the ++ portion of the loop counter
+    while (array.length > 0) {
+        sortedArr.push(findMinAndRemove(array))
+    }
+    return sortedArr
 }
